8dc1326f90bb5614c679845baca27d2a36b03611
Darshan Satbhai <darshansatbhai38@gmail.com>
2026-09-21 10:16:10 +0530

    fix: update flutter version in release.yml

---
+++ fix_flutter_version.py
@@ -0,0 +1,21 @@
+﻿filepath = r"D:\WINDECK\.github\workflows\release.yml"
+with open(filepath, "r", encoding="utf-8") as f:
+    text = f.read()
+
+old_flutter = """      - name: Setup Flutter
+        uses: subosito/flutter-action@v2
+        with:
+          flutter-version: '3.29.0'
+          channel: 'stable'
+          cache: true"""
+
+new_flutter = """      - name: Setup Flutter
+        uses: subosito/flutter-action@v2
+        with:
+          channel: 'stable'
+          cache: true"""
+
+text = text.replace(old_flutter, new_flutter)
+
+with open(filepath, "w", encoding="utf-8") as f:
+    f.write(text)
